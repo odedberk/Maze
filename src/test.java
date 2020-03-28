9e2b0c42baeff4601ce8561e03b9d5f66baee09a
eilam gal <eilamg@post.bgu.ac.il>
2020-03-28 21:49:46 +0300

    eilam test commit

--- src/test.java
+++ src/test.java
@@ -2,6 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("hi");
     }
 
 }
