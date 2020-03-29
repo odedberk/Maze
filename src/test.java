6c915ce68114dced69ecd860f4a8e58a50354018
odedberk <62807751+odedberk@users.noreply.github.com>
2020-03-29 13:55:16 +0300

    finish reverting
    removed tags

--- src/test.java
+++ src/test.java
@@ -4,12 +4,8 @@ public class test {
         System.out.println("hi");
         System.out.println("oded test commit");
         System.out.println("test2");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
         System.out.println("test pull");
->>>>>>> parent of 4694ed7... Merge remote-tracking branch 'origin/master'
     }
 
 }
