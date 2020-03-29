f8c02f923085fc2e6339f0a0d159b21ba99343d4
oded <odedodge@gmail.com>
2020-03-29 13:35:46 +0300

    Update test.java

---
+++ src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hi");
+        System.out.println("oded test commit");
+        System.out.println("test2");
+
+        System.out.println("test pull");
+    }
+
+}
